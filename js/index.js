44a6bec9aad61e70660fbc9da153de17a4964700
NickNils <130007686+NickNils@users.noreply.github.com>
2024-03-09 14:57:06 -0500

    addd paragraphs and style date input

--- js/index.js
+++ js/index.js
@@ -3,72 +3,84 @@
         image: 'assets/images/kusama.jpeg',
         song : 'assets/music/song_01.mp3',
         name : 'Yayoi Kusama',
+        description : 'Yayoi Kusama, a Japanese artist, is famed for her avant-garde polka-dotted artworks exploring themes of infinity and obsession. Her diverse creations span painting, sculpture, and installations, captivating global audiences.',
         key : 'aries',
     },
     {
         image : 'assets/images/harring.jpeg',
         song : 'assets/music/song_02.mp3',
         name : 'Keith Haring',
+        description : 'Keith Haring, an iconic American artist, rose to fame in the 1980s with his vibrant, graffiti-inspired artwork characterized by bold lines, vivid colors, and social commentary, leaving a lasting cultural legacy.',
         key : 'taurus',
     },
     {
         image : 'assets/images/basquiat.jpeg',
         song : 'assets/music/song_03.mp3',
         name : 'Jean Michelle Basquiat',
+        description : 'A trailblazing American artist known for his raw, graffiti-inspired style, Basquiats work delved into themes of race, identity, and society, leaving an indelible mark on contemporary art.',
         key : 'capricorn',
     },
     {
         image : 'assets/images/escher.jpeg',
         song : 'assets/music/song_04.mp3',
         name : 'MC Escher',
+        description : 'M.C. Escher, a Dutch artist, is renowned for his mind-bending prints exploring concepts of symmetry, infinity, and optical illusions. His meticulous drawings challenge viewers perceptions, inspiring fascination and admiration worldwide.',
         key : 'gemini',
     },
     {
         image : 'assets/images/hopper.jpeg',
         song : 'assets/music/song_05.mp3',
         name : 'Edward Hopper',
+        description : 'Renowned for his iconic portrayals of American life, Hopper captured the essence of urban and rural landscapes, often exploring themes of solitude, alienation, and the passage of time.',
         key : 'cancer',
     },
     {
         image : 'assets/images/noguchi.jpeg',
         song : 'assets/music/song_06.mp3',
         name : 'Isama Noguchi',
+        description : 'A pioneering Japanese-American sculptor and designer, Noguchis diverse body of work seamlessly blended Eastern and Western influences, reshaping the boundaries of sculpture, landscape architecture, and furniture design.',
         key : 'scorpio',
     },
     {
         image : 'assets/images/warhol.jpeg',
         song : 'assets/music/song_07.mp3',
         name : 'Andy Warhol',
+        description : 'An influential figure in the Pop Art movement, Warhol revolutionized art by elevating everyday objects and celebrities into iconic symbols through his distinctive silk-screening techniques and mass production aesthetics.',
         key : 'leo',
     },
     {
         image : 'assets/images/pollock.jpeg',
         song : 'assets/music/song_08.mp3',
         name : 'Jackson Pollock',
+        description : 'Famed for his groundbreaking "drip" painting technique, Pollocks abstract expressionist works embodied a sense of spontaneity and energy, redefining the possibilities of painting and leaving a profound impact on modern art.',
         key : 'aquarius',
     },
     {
         image : 'assets/images/mondrian.jpeg',
         song : 'assets/music/song_09.mp3',
         name : 'Piet Mondrian',
+        description : 'A leading figure in the De Stijl movement, Mondrians iconic geometric compositions explored the harmony of form, color, and space, paving the way for abstract art and influencing numerous fields of design.',
         key : 'pisces',
     },
     {
         image : 'assets/images/rothko.jpeg',
         song : 'assets/music/song_10.mp3',
         name : 'Mark Rothko',
+        description : 'Renowned for his transcendent color field paintings, Rothkos monumental canvases exude an emotional depth and spiritual intensity, inviting viewers into contemplative experiences of pure color and light.',
         key : 'libra',
     },
     {
         image : 'assets/images/kandinsky.jpeg',
         song : 'assets/music/song_11.mp3',
         name : 'Wasilly Kandinsky',
+        description : 'A pioneering Russian painter and art theorist, Kandinskys abstract compositions were imbued with spiritual and philosophical ideas, pioneering a new visual language that emphasized the emotive power of color and form.',
         key : 'sagittarius',
     },
     {
         image : 'assets/images/indiana.jpeg',
         song : 'assets/music/song_12.mp3',
         name : 'Robert Indiana',
+        description : 'Best known for his iconic "LOVE" sculpture, Indianas bold and graphic artworks explored themes of love, identity, and the American experience, leaving a lasting imprint on contemporary culture.',
         key : 'virgo',
     },
   ]
@@ -79,6 +91,8 @@ const current_image = document.getElementById('art_image');
 
 const current_title = document.getElementById('artist_title');
 
+const current_description = document.getElementById('descrip');
+
 function change_image(artist_number) {
     current_image.src = artists[artist_number].image;
 }
@@ -87,6 +101,9 @@ function change_title(artist_number) {
     current_title.textContent = artists[artist_number].name;
 }
 
+function change_description(artist_number) {
+  current_description.textContent = artists[artist_number].description;
+}
 
 // DATE INPUT STUFF ==============================================================================================================================
 
@@ -144,6 +161,7 @@ function handle_submit(event) {
       change_image(i);
       sound_file = artists[i].song;
       play_audio(music_box, sound_file);
+      change_description(i);
     }
   }
 }
@@ -185,6 +203,7 @@ function button_press() {
             change_image(i);
             sound_file = artists[i].song;
             play_audio(music_box, sound_file);
+            change_description(i);
         });
     }
 };
